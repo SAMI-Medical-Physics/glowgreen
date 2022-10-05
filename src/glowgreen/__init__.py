527a6523d9aa37b30221c4015a49e18a5c52e867
jakeforster <jake.forster91@gmail.com>
2022-10-05 21:04:57 +1030

    tues 2

--- src/glowgreen/__init__.py
+++ src/glowgreen/__init__.py
@@ -1,4 +1,11 @@
-glowgreen_version = "0.0.3"
+#"""
+import importlib.metadata
+try:
+    __version__ = importlib.metadata.version("glowgreen")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = None
+#"""
+#__version__ = "0.0.3"
 
 # import glowgreen.clearance
 from .clearance import Clearance_1m
